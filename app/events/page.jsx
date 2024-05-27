a754819959f6149a007f3f64e8647bf94b831b61
Jesper <jesper.wicklund@outlook.com>
2024-05-27 18:43:47 +0200

    Initial commit

---
+++ app/events/page.jsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function EventPage() {
+  return (
+    <div>
+        <UserButton></UserButton>
+    </div>
+  )
+}
+
+export default EventPage
