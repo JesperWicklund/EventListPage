c7c23139e1b7b9fd706d543938940b39de96ed69
Jesper <jesper.wicklund@outlook.com>
2024-05-29 12:44:58 +0200

    update need to add some styling to booked events and being able to cancel booking

--- app/(private)/min-sida/page.jsx
+++ app/(private)/min-sida/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client'
+
+
+import { useUser } from "@clerk/nextjs";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function MyPage() {
+
+ const [events, setEvents] = useState([]);
+ const { user } = useUser()
+ const attendeeEmail = user?.primaryEmailAddress?.emailAddress || ''
+ 
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/api/event`, {
+          params: {
+            attendeeEmail: attendeeEmail
+          }
+        });
+        setEvents(response.data);
+      } catch (error) {
+        console.error('Failed to fetch events:', error);
+      }
+    };
+
+    fetchEvents();
+  }, [attendeeEmail]);
+
+  const filterEvents = events.filter(event => event.attendees.includes(attendeeEmail))
+
   return (
-    <div>MyPage</div>
-  )
-}
+    <div>
+      <h1>Bokade Event</h1>
+      <ul>
+        {filterEvents.map(event => (
+          <li key={event.id}>{event.title} - {event.attendees}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default MyPage
